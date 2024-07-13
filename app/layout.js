c2615f66b333b9ab6fa8f915c4394aee186c53b1
Shibin A <a.shibin6385@gmail.com>
2024-07-13 20:42:05 +0530

    gtm

--- app/layout.js
+++ app/layout.js
@@ -28,7 +28,7 @@ export default function RootLayout({ children }) {
         </main>
         <Footer />
       </body>
-      {/* <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM} /> */}
+      <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM} />
     </html>
   );
 }
