51b3426365a58662d55fafa967d18be6a5fe7ba7
Shibin A <a.shibin6385@gmail.com>
2024-07-15 11:05:22 +0530

    'done'

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -2,13 +2,13 @@ export const experiences = [
   {
     id: 1,
     title: 'Software Developer',
-    company: "Ideaux Technologies",
+    company: "Feather Softwares",
     duration: "(March 2023 - May 2024)"
   },
   {
     id: 2,
     title: "Software Developer",
-    company: "Feather Softwares",
+    company: "Ideaux Technologies",
     duration: "(July 2022 - March 2023)"
   },
   
