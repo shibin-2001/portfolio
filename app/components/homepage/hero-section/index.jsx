c5e1f45a9825942c9d6a7b1c7480f48f19987be8
Shibin A <a.shibin6385@gmail.com>
2024-08-16 15:53:54 +0530

    updates

--- app/components/homepage/hero-section/index.jsx
+++ app/components/homepage/hero-section/index.jsx
@@ -129,8 +129,8 @@ function HeroSection() {
                 <span className="text-amber-300">MongoDB</span>
                 <span className="text-gray-400">{"', '"}</span>
                 <span className="text-amber-300">Django</span>
-                {/* <span className="text-gray-400">{"', '"}</span>
-                <span className="text-amber-300">AWS</span> */}
+                <span className="text-gray-400">{"', '"}</span>
+                <span className="text-amber-300">Flutter</span>
                 <span className="text-gray-400">{"'],"}</span>
               </div>
               <div>
